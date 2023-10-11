320637f2ec25fbbfd869685ac82b2ade67ab3f09
Adityaxd <achaudhary.chaudhary991@gmail.com>
2023-10-11 20:41:52 +0530

    Made url changes for projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -220,7 +220,7 @@ const projects = [
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Adityaxd/portfolio_website/",
   },
 ];
 
